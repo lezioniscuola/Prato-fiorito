ffddb458bcb0e576ea871eec2bd0c59416e7efeb
Leo <justload@sigmamale.club>
2024-05-17 06:20:10 +0200

    Piccoli miglioramenti

--- PratoFiorito.java
+++ PratoFiorito.java
@@ -1,5 +1,6 @@
 import java.util.Random;
 import java.util.Scanner;
+
 public class PratoFiorito {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
@@ -16,24 +17,30 @@ public class PratoFiorito {
         int[][] matrice = creamatrice(larghezza, lunghezza);
         boolean[][] matriceBoolean = Creamatriceboolean(lunghezza, larghezza);
         PosizionamentoFiori(matrice, matriceBoolean);
-        do{
+        do {
             StampaCampo(matrice, matriceBoolean, lunghezza, larghezza);
-            System.out.println("Inserisci le coordinate della tua scelta(prima la lunghezza e poi la larghezza)");
-            i=in.nextInt()-1;
-            j=in.nextInt()-1;
-            control = FlowerControl(matrice, i, j);
-            matriceBoolean=Scoperchiato(matriceBoolean, i, j);
-            vittoria=ControlloMosseDisponibili(matriceBoolean, lunghezza, larghezza);
-        }while(!control&&!vittoria);
-        if(vittoria){
+            System.out.println("Inserisci le coordinate della tua scelta(prima la colonna e poi la riga)");
+            System.out.print(">> ");
+            i = in.nextInt() - 1;
+            j = in.nextInt() - 1;
+            control = FlowerControl(matrice, matriceBoolean, i, j);
+            matriceBoolean = Scoperchiato(matriceBoolean, i, j);
+            vittoria = ControlloMosseDisponibili(matriceBoolean, lunghezza, larghezza);
+        } while (!control && !vittoria);
+        if (vittoria) {
+            System.out.println("Partita finita, ecco il campo scoperto: ");
+            gameFinished(matrice, matriceBoolean);
+            StampaCampo(matrice, matriceBoolean, lunghezza, larghezza);
             System.out.println("Hai vinto!");
-        }
-        else{
+        } else {
+            System.out.println("Partita finita, ecco il campo scoperto: ");
+            gameFinished(matrice, matriceBoolean);
+            StampaCampo(matrice, matriceBoolean, lunghezza, larghezza);
             System.out.println("Hai perso");
         }
     }
 
-    public static int[][] creamatrice(int larghezza, int lunghezza) {
+    private static int[][] creamatrice(int larghezza, int lunghezza) {
         int[][] matrice = new int[larghezza][lunghezza];
         return matrice;
     }
@@ -43,44 +50,91 @@ public class PratoFiorito {
         return booleanmatrix;
     }
 
-    public static void PosizionamentoFiori(int[][] matrice, boolean[][] matriceBoolean) {
+    private static void PosizionamentoFiori(int[][] matrice, boolean[][] matriceBoolean) {
         Random random = new Random();
         for (int i = 0; i < matrice.length; i++) {
             for (int j = 0; j < matrice[i].length; j++) {
-                if (random.nextInt(4) == 1) {
+                if (random.nextInt(7) == 1) {
                     matrice[i][j] = 9;
-                    matriceBoolean[i][j] = true;
+                    //matriceBoolean[i][j] = true;
                     incrementoAdiacenti(matrice, i, j);
                 }
             }
         }
     }
-    public static void StampaCampo (int[][]matrice, boolean[][]matriceBoolean, int lunghezza, int larghezza){
-        for (int i=0;i<larghezza;i++){
-            for (int j=0;j<lunghezza;j++){
+
+    private static void StampaCampo(int[][] matrice, boolean[][] matriceBoolean, int lunghezza, int larghezza) {
+        System.out.print("   ");
+        for (int i = 0; i < larghezza; i++) {
+            System.out.print((i + 1) + "  ");
+        }
+        System.out.println();
+        for (int i = 0; i < larghezza; i++) {
+            for (int j = 0; j < lunghezza; j++) {
                 char quadrato = 9632;
-                if(matriceBoolean[i][j]==true&&matrice[i][j]!=9) {
-                    System.out.print(matrice[i][j] + "  ");
+                if (j == 0 && i < 9) {
+                    System.out.print((i + 1) + "  ");
                 }
-                else{
+                else if (j == 0 && i >= 9) {
+                    
+                    System.out.print((i + 1) + " ");
+                }
+                if (matriceBoolean[i][j] == true) {
+                    System.out.print(matrice[i][j] + "  ");
+                } else {
                     System.out.print(quadrato + "  ");
                 }
             }
             System.out.println();
         }
     }
-    public static boolean FlowerControl(int [][] matrice, int i, int j){
-        boolean controllo=false;
-        if(matrice[i][j]==9)
+
+    private static boolean FlowerControl(int[][] matrice, boolean[][] matriceBoolean, int i, int j) {
+        boolean controllo = false;
+        if (matrice[i][j] == 9)
             return true;
-        else
-            return false;
+        else if (matrice[i][j] == 0)
+            checkZeros(matrice, matriceBoolean, i, j);
+        return false;
     }
-    public static boolean ControlloMosseDisponibili(boolean[][] matriceBoolean, int lunghezza, int larghezza ) {
+
+    private static void checkZeros(int[][] matrice, boolean[][] matriceBoolean, int i, int j) {
+        if (matrice[i][j] == 0) {
+            matriceBoolean[i][j] = true;
+            for (int x = -1; x <= 1; x++) {
+                for (int y = -1; y <= 1; y++) {
+                    if (x == 0 && y == 0)
+                        continue;
+                    int new_x = i + x;
+                    int new_y = j + y;
+                    if (new_x >= 0 && new_x < matrice.length && new_y >= 0 && new_y < matrice[i].length
+                            && matriceBoolean[new_x][new_y] == false) {
+                        try {
+                            checkZeros(matrice, matriceBoolean, new_x, new_y);
+                        } catch (StackOverflowError e) {
+                            matriceBoolean[i][j] = true;
+                        }
+                    }
+                }
+            }
+        } else {
+            matriceBoolean[i][j] = true;
+        }
+    }
+
+    private static void gameFinished(int[][] matrice, boolean[][] matriceBoolean) {
+        for (int x = 0; x < matrice.length; x++) {
+            for (int y = 0; y < matrice[x].length; y++) {
+                matriceBoolean[x][y] = true;
+            }
+        }
+    }
+
+    private static boolean ControlloMosseDisponibili(boolean[][] matriceBoolean, int lunghezza, int larghezza) {
         boolean vittoria = true;
         for (int i = 0; i < larghezza; i++) {
             for (int j = 0; j < lunghezza; j++) {
-                if (matriceBoolean[i][j]==false) {
+                if (matriceBoolean[i][j] == false) {
                     vittoria = false;
                     break;
                 }
@@ -88,21 +142,25 @@ public class PratoFiorito {
         }
         return vittoria;
     }
-    public static boolean[][] Scoperchiato(boolean[][]matriceBoolean, int i, int j){
-        matriceBoolean[i][j]=true;
+
+    private static boolean[][] Scoperchiato(boolean[][] matriceBoolean, int i, int j) {
+        matriceBoolean[i][j] = true;
         return matriceBoolean;
     }
-    public static void incrementoAdiacenti(int[][] matrice, int i, int j){
+
+    private static void incrementoAdiacenti(int[][] matrice, int i, int j) {
         for (int x = -1; x <= 1; x++) {
             for (int y = -1; y <= 1; y++) {
                 if (x == 0 && y == 0)
                     continue;
                 int leftRight = i + x;
                 int upDown = j + y;
-                if (leftRight >= 0 && leftRight < matrice.length && upDown >= 0 && upDown < matrice[i].length && matrice[leftRight][upDown] != 9) {
+                if (leftRight >= 0 && leftRight < matrice.length && upDown >= 0 && upDown < matrice[i].length
+                        && matrice[leftRight][upDown] != 9) {
                     matrice[leftRight][upDown]++;
                 }
             }
         }
     }
+
 }
